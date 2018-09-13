9329104d3ff411c37eac62ff7ba82ea1d1c46513
Michael Robinson <seraph890@gmail.com>
2018-09-13 23:35:31 +1000

    added a basic matrix and some git ignore things

--- src/board.rs
+++ src/board.rs
@@ -1,3 +1,6 @@
+use na::{U2, U3, Dynamic, MatrixArray, MatrixVec};
+use typenum::U1000;
+
 pub struct Board {
     pub height: usize,
     pub width: usize,
@@ -8,9 +11,10 @@ impl Board {
         Board { width: width, height: height }
     }
 
-    // fn build() {
-    //
-    // }
+    fn build() {
+        type Matrix2x3f = Matrix<f32, U2, U3, MatrixArray<f32, U2, U3>>;
+        let b = Matrix2x3::from_fn(|r, c| (r + 1) as f32 + (c + 1) as f32 / 10.0);
+    }
 
     // fn get_cell(&self, position: &Place) -> Option<Cell> {
     // }
